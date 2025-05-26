25e090fb1fb533d8ef553bd09d494bfa7c329b12
Nurullah649 <113285824+Nurullah649@users.noreply.github.com>
2025-05-26 15:12:43 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type React from "react"
 import ClientLayout from "./ClientLayout"
-
+import { SpeedInsights } from '@vercel/speed-insights/next';
 export const metadata = {
   title: "Nurullah Kurnaz | Kişisel Portfolyo",
   description: "Nurullah Kurnaz - Konya Teknik Üniversitesi Bilgisayar Mühendisliği öğrencisi. Yapay Zeka ve Robotik alanındaki projelerim ve deneyimlerim.",
@@ -8,7 +8,7 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return <ClientLayout>{children}</ClientLayout>
+  return <ClientLayout>{children}<SpeedInsights /></ClientLayout>
 }
 
 
